06ccdd2e23b44f4baf7e6374ccb31f058c0c48b7
Tero Kadenius <tero.kadenius@flowa.fi>
2022-02-11 13:32:52 +0200

    Adds config for mac and some usability util stuff

--- .emacs.d/flowa/usability.el
+++ .emacs.d/flowa/usability.el
@@ -50,8 +50,36 @@
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
+;; --------------------------
+;; Helpers for copying lines
+;; --------------------------
+
 ;; duplicates line C-c d
 (global-set-key (kbd "C-c d") 'duplicate-line)
+
+(defun copy-curent-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+    (interactive "p")
+    (let ((beg (line-beginning-position))
+          (end (line-end-position arg)))
+      (when mark-active
+        (if (> (point) (mark))
+            (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+          (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+      (if (eq last-command 'copy-line)
+          (kill-append (buffer-substring beg end) (< end beg))
+        (kill-ring-save beg end)))
+    (kill-append "\n" nil)
+    (beginning-of-line (or (and arg (1+ arg)) 2))
+    (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+;; M-n copies current line (= adds to kill ring)
+(global-set-key (kbd "M-n") 'copy-current-line)
 
 ;; --------------------------
 ;; Better defaults from Helm
